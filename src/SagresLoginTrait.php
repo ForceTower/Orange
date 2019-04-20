674f100384514608dae9e20753c4bc5ff0fb621f
João Paulo Sena <joaopaulo761@gmail.com>
2019-04-20 16:30:11 -0300

    :sparkles: Creates account when none is found

--- src/SagresLoginTrait.php
+++ src/SagresLoginTrait.php
@@ -6,7 +6,8 @@ namespace ForceTower\UPassportLogin;
 use GuzzleHttp\Client;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use League\OAuth2\Server\Exception\OAuthServerException;
 use Throwable;
 
@@ -49,12 +50,41 @@ trait SagresLoginTrait
                     $content = $response->getBody()->getContents();
                     $html = str_get_html($content);
                     $name = $html->find('span[class=usuario-nome]');
+                    // If the trait is able to verify the user
                     if (count($name) > 0) {
-                        Log::info('The extracted name is '.$name[0]);
-                        $userModel = config('auth.providers.users.model');
+                        // Find config fields
+                        $userModel              = config('auth.providers.users.model');
                         $sagres_username_column = config('sagres.registration.username', 'username');
-                        $user = $userModel::where($sagres_username_column, $username . '_' . $institution)->first();
-                        Log::info('Model that will be returned '.$user);
+                        $sagres_password_column = config('sagres.registration.password', 'password');
+                        $sagres_email_column    = config('sagres.registration.email', 'email');
+                        $sagres_create_account  = config('sagres.registration.create_account', true);
+
+                        // Find the user
+                        $user = null;
+                        // If it contains a @ it means user connected with email
+                        $emailConnected = strpos($username, '@') !== false;
+                        if ($emailConnected) {
+                            $user = $userModel::where($sagres_email_column, $username)->first();
+                        } else {
+                            $user = $userModel::where($sagres_username_column, $username . '_' . $institution)->first();
+                        }
+
+                        // If user is not registered yet, create it with minimal information
+                        if (!$user && $sagres_create_account) {
+                            $user = new $userModel();
+
+                            if ($emailConnected) {
+                                $user->{$sagres_email_column} = $username;
+                                $newUsername = explode('@', $username)[0];
+                                $user->{$sagres_username_column} = $newUsername . '_' . $institution . '_email';
+                            } else {
+                                $user->{$sagres_username_column} = $username . '_' . $institution;
+                            }
+
+                            $user->{$sagres_password_column} = Hash::make($password);
+                            $user->save();
+                        }
+
                         return $user;
                     }
                 }
--- src/config/sagres.php
+++ src/config/sagres.php
@@ -16,7 +16,9 @@ return [
         ]
     ],
     'registration' => [
-        'username' => 'system_username'
+        'create_account' => true,
+        'username' => 'username',
+        'password' => 'password'
     ],
     'headers' => [
         'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
